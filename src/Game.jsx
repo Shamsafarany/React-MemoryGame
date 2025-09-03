5c3cf952073683e02214b70a7a3ce69f890023eb
Sham Safarany <125689014+Shamsafarany@users.noreply.github.com>
2025-09-03 14:59:15 +0300

    add fetch API

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,9 +1,17 @@
 import Card from "./Card";
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 function Game(){
 
     const [currentScore, setCurrentScore] = useState(0);
     const [best, setBest] = useState(0);
+    const [cards, setCards] = useState([]);
+
+     useEffect(() => {
+       fetch("https://pokeapi.co/api/v2/pokemon/")
+         .then((response) => response.json())
+         .then((data) => console.log(data))
+         .catch((e) => console.log(e));
+     }, []);
 
     function handleIncrementCurrent(){
         setCurrentScore(currentScore => currentScore + 1);
@@ -13,16 +21,7 @@ function Game(){
             setBest(currentScore);
         }
     }
-
-    function callAPI(){
-        fetch("https://pokeapi.co/api/v2/pokemon/ditto")
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(e => console.log(e));
-    }
-    callAPI();
-
-
+   
     return (
       <div className="gameContainer">
         <div className="header">
@@ -31,8 +30,12 @@ function Game(){
             <h3>DO NOT choose the same card or you lose!</h3>
           </div>
           <div className="scoreContainer">
-            <p>Current Score: {currentScore}</p>
-            <p>Best Score: {best}</p>
+            <p>
+              Current Score: <strong>{currentScore}</strong>
+            </p>
+            <p>
+              Best Score: <strong>{best}</strong>
+            </p>
           </div>
         </div>
 
